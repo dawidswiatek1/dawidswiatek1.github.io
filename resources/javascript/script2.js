47b4fb882aee021effe895331a093692dc63b997
dawidswiatek1 <161316497+dawidswiatek1@users.noreply.github.com>
2024-05-09 14:22:55 +0200

    Delete resources directory

--- resources/javascript/script2.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-  }
-  
-  document.getElementById('callButton').addEventListener('click', function() {
-    window.location.href = 'tel:NUMER_TELEFONU';
-  });
